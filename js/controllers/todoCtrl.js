b2883976550d26f162fb4bfc5a29a736aa19783e
zoetrope <a.ikezoe@gmail.com>
2014-01-18 01:13:32 +0900

    apply reactiveproperty-angular

--- js/controllers/todoCtrl.js
+++ js/controllers/todoCtrl.js
@@ -7,76 +7,94 @@
  * - exposes the model to the template and provides event handlers
  */
 todomvc.controller('TodoCtrl', function TodoCtrl($scope, $routeParams, todoStorage, filterFilter) {
-	var todos = $scope.todos = todoStorage.get();
+    $scope.todos = new rxprop.ReactiveCollection($scope, todoStorage.get());
 
-	$scope.newTodo = '';
-	$scope.editedTodo = null;
+    $scope.newTodo = '';
+    $scope.editedTodo = null;
 
-	$scope.$watch('todos', function (newValue, oldValue) {
-		$scope.remainingCount = filterFilter(todos, { completed: false }).length;
-		$scope.completedCount = todos.length - $scope.remainingCount;
-		$scope.allChecked = !$scope.remainingCount;
-		if (newValue !== oldValue) { // This prevents unneeded calls to the local storage
-			todoStorage.put(todos);
-		}
-	}, true);
+    $scope.todos
+        .subscribe(function (values) {
+            todoStorage.put(values);
+        });
 
-	// Monitor the current route for changes and adjust the filter accordingly.
-	$scope.$on('$routeChangeSuccess', function () {
-		var status = $scope.status = $routeParams.status || '';
+    $scope.remainingCount = $scope.todos
+        .select(function (values) {
+            return filterFilter(values, { completed: false }).length;
+        }).toReactiveProperty($scope);
 
-		$scope.statusFilter = (status === 'active') ?
-			{ completed: false } : (status === 'completed') ?
-			{ completed: true } : null;
-	});
+    $scope.completedCount = $scope.todos
+        .select(function (values) {
+            return values.length - $scope.remainingCount.value;
+        })
+        .toReactiveProperty($scope);
 
-	$scope.addTodo = function () {
-		var newTodo = $scope.newTodo.trim();
-		if (!newTodo.length) {
-			return;
-		}
+    $scope.allChecked = $scope.remainingCount
+        .select(function (value) {
+            return !value;
+        })
+        .toReactiveProperty($scope);
 
-		todos.push({
-			title: newTodo,
-			completed: false
-		});
+    $scope.statusFilter = $scope.$onAsObservable('$routeChangeSuccess')
+        .select(function (_) {
+            var status = $scope.status = $routeParams.status || '';
+            return (status === 'active') ?
+            { completed: false } :
+                (status === 'completed') ? { completed: true } : null;
+        }).toReactiveProperty($scope);
 
-		$scope.newTodo = '';
-	};
+    $scope.addTodo = new rxprop.ReactiveCommand($scope);
+    $scope.addTodo
+        .select(function (_) {
+            return $scope.newTodo.trim();
+        })
+        .where(function (newTodo) {
+            return newTodo.length !== 0;
+        })
+        .subscribe(function (newTodo) {
+            $scope.todos.add({
+                title: newTodo,
+                completed: false
+            });
+            $scope.newTodo = '';
+        });
 
-	$scope.editTodo = function (todo) {
-		$scope.editedTodo = todo;
-		// Clone the original todo to restore it on demand.
-		$scope.originalTodo = angular.extend({}, todo);
-	};
+    $scope.editTodo = new rxprop.ReactiveCommand($scope, function (todo) {
+        $scope.editedTodo = todo;
+        $scope.originalTodo = angular.extend({}, todo);
+    });
 
-	$scope.doneEditing = function (todo) {
-		$scope.editedTodo = null;
-		todo.title = todo.title.trim();
+    $scope.doneEditing = new rxprop.ReactiveCommand($scope);
+    $scope.doneEditing
+        .select(function (todo) {
+            $scope.editedTodo = null;
+            todo.title = todo.title.trim();
+            return todo;
+        })
+        .where(function (todo) {
+            return todo.title === "";
+        })
+        .subscribe(function (todo) {
+            $scope.removeTodo.execute(todo);
+        });
 
-		if (!todo.title) {
-			$scope.removeTodo(todo);
-		}
-	};
+    $scope.revertEditing = new rxprop.ReactiveCommand($scope, function (todo) {
+        $scope.todos.update(todo, $scope.originalTodo);
+        $scope.doneEditing.execute($scope.originalTodo);
+    });
 
-	$scope.revertEditing = function (todo) {
-		todos[todos.indexOf(todo)] = $scope.originalTodo;
-		$scope.doneEditing($scope.originalTodo);
-	};
+    $scope.removeTodo = new rxprop.ReactiveCommand($scope, function (todo) {
+        $scope.todos.remove(todo);
+    });
 
-	$scope.removeTodo = function (todo) {
-		todos.splice(todos.indexOf(todo), 1);
-	};
+    $scope.clearCompletedTodos = new rxprop.ReactiveCommand($scope, function (_) {
+        $scope.todos.values = $scope.todos.values.filter(function (val) {
+            return !val.completed;
+        });
+    });
 
-	$scope.clearCompletedTodos = function () {
-		$scope.todos = todos = todos.filter(function (val) {
-			return !val.completed;
-		});
-	};
-
-	$scope.markAll = function (completed) {
-		todos.forEach(function (todo) {
-			todo.completed = !completed;
-		});
-	};
+    $scope.markAll = new rxprop.ReactiveCommand($scope, function (completed) {
+        $scope.todos.values.forEach(function (todo) {
+            todo.completed = !completed;
+        });
+    });
 });
